23a3751be93a4859f4924abb7c9453136002af13
ehsankorhani <ehsan.korhani@practifi.com>
2021-05-31 10:16:03 +1000

    pubsub

---
+++ app.js
@@ -0,0 +1,6 @@
+const publisher = require('./publisher');
+const subscriber = require('./subscriber');
+
+// We use moduleA's publishEvent() method
+publisher.publishEvent();
+publisher.publishEvent();
